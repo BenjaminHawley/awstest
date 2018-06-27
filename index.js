edca824f3a597030e4f71a14ff8211bd9c3baa97
BenjaminHawley <benhawley0@gmail.com>
2018-06-26 20:07:04 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+// app.get('/P2P', (req, res) => {
+//   res.sendFile()
+// })
+
+app.listen(2000, () => console.log('app is listening on port 3000'));
