9927a170c1bf0f17b1bc02392ff1d75473ce48dd
BenjaminHawley <benhawley0@gmail.com>
2018-06-26 20:14:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
 //   res.sendFile()
 // })
 
-app.listen(2000, () => console.log('app is listening on port 3000'));
+app.listen(2000, () => console.log('app is listening on port 2000'));
